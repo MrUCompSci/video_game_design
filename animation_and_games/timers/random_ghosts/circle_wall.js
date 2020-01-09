b799fdafac6b690c9fbfef948d6f0cc100c2cb17
MrUCompSci <58056495+MrUCompSci@users.noreply.github.com>
2020-01-09 10:30:32 -0500

    Create circle_wall.js

---
+++ animation_and_games/timers/random_ghosts/circle_wall.js
@@ -0,0 +1,33 @@
+// Constants
+var RADIUS = 20;
+var DELAY = 40;
+
+// Define your global variables here
+//You should have 3 more (counter, x, y) (5 total including above)
+
+function start(){
+    //Set a timer, call draw, and add a delay
+	
+}
+
+//Create draw function
+    //This function will draw from left to right, then top to bottom
+
+    //Create a circle
+    
+    //Set a circle position
+    
+    /*Create an if statements to determine if you should make 
+    the circle red or black (use modulos - think even or odd)*/
+    
+    //Add circle
+    
+    //Increment counter
+    
+    //Modify x position
+    
+    /* Create an if stameent to see if xposition is outside width
+    if it is, reset x position and increase y position*/
+    
+    /*Create an if statement for if the y position is greater then the height
+    if it is, stop the animation*/

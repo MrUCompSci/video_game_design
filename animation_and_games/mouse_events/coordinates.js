6593540324e9143cb750f0b6acc021e9d891d607
MrUCompSci <58056495+MrUCompSci@users.noreply.github.com>
2020-01-15 07:11:38 -0500

    Create coordinates.js

---
+++ animation_and_games/mouse_events/coordinates.js
@@ -0,0 +1,19 @@
+/* This program displays the x and y
+ * coordinates in a label on the screen
+ * and updates when the mouse moves */
+ 
+ //Create a variable text
+ 
+function start(){
+   //Set text to a new variable Text with "" as the argument
+   
+   //Set the text position to 0 and the top of text
+   
+   //Add the text
+   
+   /*Add a move mouse method with your move 
+   function as an argument*/
+}
+
+/*Create a function move with parameter e
+set the text of text variable to the x and y of e*/

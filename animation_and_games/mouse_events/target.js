5e108367813189e1770fd57fdaa62222e3f4e09a
MrUCompSci <58056495+MrUCompSci@users.noreply.github.com>
2020-01-15 07:24:06 -0500

    Create target.js

---
+++ animation_and_games/mouse_events/target.js
@@ -0,0 +1,38 @@
+// This program draws a target that follows the mouse
+
+//Create variables for horizontal line, and verticle line
+
+function start(){
+  //Assign the above variables to new Lines with 0 for all arguments.
+  
+  //Use your setHorizontalLine function with argument half the height
+  
+  //Use your setVerticleLine function with argument half the width
+  
+  //Add your horizontal line
+  
+  //Add your verticle line
+  
+  //Add your mouse move method with argument resetTarget function
+  
+  //Add your mouse click method with argument addRedCircle function
+	
+}
+
+/*Create an addRedCircle function with parameter e
+create a new circle with radius 10
+set the color to red
+set the position to x and y of e
+add circle*/
+
+/*Create a function with setHorizontalLine with parameter y
+set horizontal line position to 0 and y
+set horizontal line endpoint to width,y*/
+
+/*Create a function with setVerticleLine with parameter x
+set verticle line to position to x and 0
+set verticle line endpoint to x and height*/
+
+/*Create a function with resetTarget with prameter e
+setHorizontalLine to x position of e
+setVerticleLine to y position of e*/

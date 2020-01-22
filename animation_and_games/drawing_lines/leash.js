e0c258dbffc75b9b9939bb83f79092bc0bcd1df4
MrUCompSci <58056495+MrUCompSci@users.noreply.github.com>
2020-01-22 07:20:08 -0500

    Create leash.js

---
+++ animation_and_games/drawing_lines/leash.js
@@ -0,0 +1,27 @@
+var BALL_RADIUS = 30;
+
+/*Create 2 new empty variables for ball 
+and line (meaning don't assign it to anything)*/
+
+function start() {
+
+  //Create a new circle assigning it to the ball variable
+
+  //Set the ball position to the middle of the screen
+
+  //Set the ball color to yellow
+  
+  /*Create 2 new veriables for the center of the screen
+  one for x and one for y */
+  
+  //Add the line and ball
+  
+  /*Call the mouse move method with your leash function 
+  as an argument*/
+	
+}
+
+//Create a function leash with e as a parameter
+
+  //Set endpoint of the line to x and y of e
+  //Set position of ball to x and y of e

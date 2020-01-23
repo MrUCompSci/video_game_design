0da72130c7df03e34844314653c9ddd106913994
MrUCompSci <58056495+MrUCompSci@users.noreply.github.com>
2020-01-23 07:30:28 -0500

    Create basic_snake.js

---
+++ animation_and_games/key_events/basic_snake.js
@@ -0,0 +1,60 @@
+var SNAKE_WIDTH = 40;
+var SNAKE_HEIGHT = 40;
+var SNAKE_COLOR = Color.green;
+
+// Constants to represent the directions
+var EAST = 0;
+var SOUTH = 1;
+var WEST = 2;
+var NORTH = 3;
+
+//Create en empty global variable for rectangle
+
+//Create a global variable for direction and set it to EAST
+
+function start(){
+  /*Set your global variable rectangle to new rectangle
+  us the width and height variable for snake as width and height*/
+  
+  //Set rectangle color to green
+  
+  /*Set position to
+  x - half the width of screen minus half the width of snake
+  y - half the height of the screen minus half height of snake
+  */
+  
+  //Add your rectangle
+  
+  /*Set a timer calling your move function
+  set delay to 40 ms*/
+  
+  /*Call your key down method with
+  changeDir as an argument*/
+}
+
+//Create a fuction changeDir with argument e
+  /*Create an if statement, where if keycode is 
+  keyboard left, change dir to WEST*/
+  
+  /*Create an if statement, where if keycode is 
+  keyboard right, change dir to EAST*/
+  
+  /*Create an if statement, where if keycode is 
+  keyboard up, change dir to NORTH*/
+  
+  /*Create an if statement, where if keycode is 
+  keyboard down, change dir to SOUTH*/
+  
+//Create function called move
+  /*Create an if statement, where if dir is EAST
+  move the rect to position 5,0.*/
+  
+  /*Create an if statement, where if dir is WEST
+  move the rect to position -5,0.*/
+  
+  /*Create an if statement, where if dir is NORTH
+  move the rect to position 0,-5.*/
+  
+  /*Create an if statement, where if dir is SOUTH
+  move the rect to position 0,5.*/
+

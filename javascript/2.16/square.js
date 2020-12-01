32735e4454ab2f9cdbf069b947086ff64151f1cb
MrUCompSci <58056495+MrUCompSci@users.noreply.github.com>
2020-12-01 09:30:48 -0500

    Create square.js

---
+++ javascript/2.16/square.js
@@ -0,0 +1,12 @@
+function start(){
+    //Call function with 5 as an argument to test.
+	
+}
+
+//Create function OUTSIDE start()
+//Name it square with 1 parameter
+
+    //Inside the function: Multiply x by itself and save in variable
+    
+    //Inside the function: Print variable
+    

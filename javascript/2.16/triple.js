e64a989ed6f14922502ca9ac554c992cbced5d2b
MrUCompSci <58056495+MrUCompSci@users.noreply.github.com>
2020-12-01 09:30:22 -0500

    Create triple.js

---
+++ javascript/2.16/triple.js
@@ -0,0 +1,11 @@
+function start(){
+    //Call function with argument 3 to test
+
+}
+
+
+//Create function triple with parameter x
+
+    //Inside the Function: Store x * 3 in variable result
+
+    //Inside the Function: Print result

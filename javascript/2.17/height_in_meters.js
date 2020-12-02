392e8997763903b39de7dd1879eb1cca37fda085
MrUCompSci <58056495+MrUCompSci@users.noreply.github.com>
2020-12-02 08:25:54 -0500

    Create height_in_meters.js

---
+++ javascript/2.17/height_in_meters.js
@@ -0,0 +1,35 @@
+var INCHES_TO_CM = 2.54;
+var CM_TO_METERS = 0.01;
+var FEET_TO_INCHES = 12;
+
+function start(){
+    //Call your function with 6 and 4 as arguments to test.
+    
+}
+
+/*Create a function OUTSIE start() and name it
+convertHeightToMeters, give it two parameters.*/
+
+    //INSIDE the function do the following
+    
+    /*Create a new variable feet_in_inches and convert the feet to inches
+    given the global variable FEET_TO_INCHES at the top and your parameter for feet.*/
+    
+    
+    /*Create a new variable total_inches and add your parmeter for inches
+    and feet_in_inches together*/
+    
+    
+    /*create a new variable total_centimeters convert total_inches to centimeters
+    using the global variable INCHES_TO_CM at the top*/
+    
+    
+    /*Create a new variable total_meters and convert total_centimeters to meters
+    using the global variable CM_TO_METERS at the top*/
+    
+    
+    /*Print your varible total_meters*/
+    
+    
+    /*Note - this can be done in significantly less lines and with less variables,
+    can you challenge yourself to try that?*/

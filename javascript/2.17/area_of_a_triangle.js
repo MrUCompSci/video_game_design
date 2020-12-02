74a1cbfdd27437aa5a8b5dc8ca0ccf448ee44c15
MrUCompSci <58056495+MrUCompSci@users.noreply.github.com>
2020-12-02 08:12:42 -0500

    Create area_of_a_triangle.js

---
+++ javascript/2.17/area_of_a_triangle.js
@@ -0,0 +1,16 @@
+function start(){
+    //Call function with 5 and 4 as an argument to test.
+    
+}
+
+//Create a function OUTSIDE start()
+//Name it triangleArea and give it two parameters
+
+    /*Inside the function create a new variable area and set it 
+    equal to the first variable multiplied by the second then multiplied
+    by 1/2 (the formula for triangle area)*/
+    
+    
+    //Print the variable on a new line
+    
+}

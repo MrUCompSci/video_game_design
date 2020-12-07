eb82e7a569cdd17f32b2d21ee8f92d45255a45f3
MrUCompSci <58056495+MrUCompSci@users.noreply.github.com>
2020-12-07 09:25:41 -0500

    Create horizontal_lines.js

---
+++ javascript/2.18/horizontal_lines.js
@@ -0,0 +1,21 @@
+function start(){
+    //Call your function with 100,200 as arguments to test
+	  
+	  //Call your function with 200,100 as arguments to test
+       
+    //Call your function with 300,20 as arguments to test
+    
+}
+
+function horizontalLine(yLocation, length){
+    /*Create a new variable line and set it equal to new Line
+    with arguments:
+    1st: starting x location
+    2nd: starting y location
+    3rd: ending x location
+    4th: ending y location*/
+    
+    
+    //Add line
+    
+}

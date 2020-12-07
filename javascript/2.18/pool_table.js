73dd56a6e9e1bd29adbefc1c1ca6947780658c0c
MrUCompSci <58056495+MrUCompSci@users.noreply.github.com>
2020-12-07 09:55:20 -0500

    Create pool_table.js

---
+++ javascript/2.18/pool_table.js
@@ -0,0 +1,36 @@
+var POOL_BALL_RADIUS = 40;
+var POOL_BALL_FONT = "30pt Arial";
+
+function start(){
+	drawPoolBall(Color.orange, 5, 100, 100);
+	drawPoolBall(Color.green, 6, 50, 200);
+	drawPoolBall(Color.red, 3, 150, 350);
+	drawPoolBall(Color.blue, 2, 250, 140);
+
+	// Add some more pool balls!
+}
+
+
+function drawPoolBall(color, num, x, y){
+    /*Create a new variable circle and set it equal to new Circle
+    with an argument of the pool ball radius global variable*/
+	
+	//Set the color of the circle with argument color
+	
+	//Set the position of the circle with arguments x,y
+	
+	//Add Circle
+	
+	
+	/*Create a new variable txt and set it equal to new Text with arguments
+	num and the pool ball font global variable*/
+	
+	/*Set the position of txt with argumetns
+	1st: x minus half the width of txt
+	2nd: y + half the height of txt*/
+    
+    //Set the color of txt to Color.white
+    
+    //Add txt
+    
+}

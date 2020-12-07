9ec9f2b5441524bf5f5633952616121dcc1f5b81
MrUCompSci <58056495+MrUCompSci@users.noreply.github.com>
2020-12-07 09:44:42 -0500

    Create graphics_stop_light.js

---
+++ javascript/2.18/graphics_stop_light.js
@@ -0,0 +1,70 @@
+var LIGHT_RADIUS = 35;
+var STOPLIGHT_WIDTH = 120;
+var STOPLIGHT_HEIGHT = 350;
+var BUFFER = 100;
+var GRAY_COLOR = "#737071";
+
+
+
+function start(){
+    /*Call drawRectangle with the following arguments
+    1st: global variable for stop light width
+    2nd: global variable for stop light height
+    3rd: global variable for gray color
+    4th: half the width of the screen minus half the width of stop light width 
+    5th: half the height of the screen minus half the height of stop light height*/
+    
+    
+    /*Yellow Circle - call drawCircle with arguments
+    1st: global radius for light radius
+    2nd: Color.yellow
+    3rd: Half the width of the screen
+    4th: Half the height of the screen*/
+    
+    
+    /*Red Circle - call drawCircle with arguments
+    1st: global radius for light radius
+    2nd: Color.red
+    3rd: Half the width of the screen
+    4th: Half the height of the screen minus BUFFER*/
+    
+    
+    /*Green Circle - call drawCircle with arguments
+    1st: global radius for light radius
+    2nd: Color.green
+    3rd: Half the width of the screen
+    4th: Half the height of the screen plus BUFFER*/
+    
+}
+
+/*Create a function called drawRectangle with Parameters
+width, height, color, xPos, yPos*/
+
+    //Inside the function do the following...
+    /*Create a new variable rect and set it equal to new Rectangle with arguments
+    width, height*/
+    
+    //Set color of rect with argument color
+    
+    //Set position of rect with arguments xPos, yPos
+    
+    
+    //Add rect
+    
+}
+
+/*Create a function called drawRectangle with Parameters
+radius, color, xPos, yPos*/
+
+    //Inside the function do the following...
+    /*Create a new variable circle and set it equal to new Circle with
+    argument radius*/
+    
+    //Set color of circle with argument color
+    
+    //Set position of circle with arguments xPos, yPos
+    
+    
+    //Add circle
+    
+}

4f353c89a8ac4ff7d9699ec055017f37f94362d5
MrUCompSci <58056495+MrUCompSci@users.noreply.github.com>
2020-12-08 08:54:23 -0500

    Create triple_return.js

---
+++ javascript/2.19/triple_return.js
@@ -0,0 +1,10 @@
+function start(){
+    //Print the result of calling triple with 2 as an argument to test
+	
+}
+
+//Create a function called triple with a parameter x
+
+    //INSIDE that function...
+    //Return the value of x times 3
+    

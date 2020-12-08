0525d21a2499a8207e20038686da46ae3d45386f
MrUCompSci <58056495+MrUCompSci@users.noreply.github.com>
2020-12-08 08:53:34 -0500

    Create sqaure_return.js

---
+++ javascript/2.19/sqaure_return.js
@@ -0,0 +1,10 @@
+function start(){
+    //Print the result of square(5);
+	
+}
+
+//Create a function called square with a parameter x
+
+    //INSIDE the function...
+    //Return the value of x times itself
+    ==

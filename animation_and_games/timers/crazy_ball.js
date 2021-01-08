25b7b8f825a0dc949434b17d7e22d7542f474bc5
MrUCompSci <58056495+MrUCompSci@users.noreply.github.com>
2021-01-08 08:28:07 -0500

    Create crazy_ball.js

---
+++ animation_and_games/timers/crazy_ball.js
@@ -0,0 +1,44 @@
+var RADIUS = 100;
+
+/*Create a new variable circle, 
+you don't need to set it equal to anything*/
+
+
+function start(){
+    /*Set the circle variable equal to a new
+    circle, using the method, with the global variable
+    RADIUS as the argument*/
+    
+    
+    /*Set the position of circle to the middle
+    of the screen*/
+    
+    
+    //Add circle to the screen
+
+
+    /*Set a new timer with your crazy function (made below),
+    and 100 as the two arguments*/
+
+}
+
+/*Create a new function called crazy, it does
+not need any parameteres*/
+
+    //INSIDE the function...
+    /*Create a new variable called x and set it equal to a 
+    random integer between RADIUS and the width of the screen
+    minus the RADIUS*/
+    
+    
+    /*Create a new variable called y and set it equal to a
+    random integer between RADIUS and the height of the screen
+    minus the RADIUS*/
+    
+    
+    //Set the position of the circle to x and y
+    
+    
+    /*Set the color of the circle to a random color
+    using the randomizer method*/
+    

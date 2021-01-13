2a88a3f12ab9589207def7349694cfea9f88566f
MrUCompSci <58056495+MrUCompSci@users.noreply.github.com>
2021-01-13 07:09:18 -0500

    Create paint_splatter.js

---
+++ animation_and_games/random_circles/paint_splatter.js
@@ -0,0 +1,48 @@
+ // Constants
+ var CIRCLES_PER_SPLATTER = 20;
+ var MIN_RADIUS = 5;
+ var MAX_RADIUS = 25;
+ var DELAY = 500;
+
+/* This program creates a paint splatter.
+ * It's art. */
+function start(){
+    /*Set a timer with your drawPaintSplatter function and
+    global variable DELAY as the arguments*/
+    
+}
+
+/*Create a new function called drawPaintSplatter with
+no parameters*/
+
+    //INSIDE THAT FUNCTION
+    /*Create a new variable color and set it equal
+    to a random color using the randomizer*/
+    
+    
+    /*Create a for loop that starts at zero and goes up to 
+    the blobal vairbale CIRCLES_PER_SPLATTER*/
+        //INSIDE THE FOR LOOP
+        
+        /*Call your drawCircle function with arguments
+        -Random integer between globals MIN_RADIUS and MAX_RADIUS
+        -color
+        -Random integter between 0 and width of screen
+        -Random integer between 0 and height of screen
+
+
+/*Create a new function called drawCircle with parameters
+radius, color, x, y*/
+
+    //INSIDE THE FUNCTION
+    /*Create a new variable called circle and set
+    it equal to a new Circle with argument radius*/
+    
+    
+    //Set color of circle to color
+    
+    
+    //Set position of circle to x,y
+    
+    
+    //Add circle to screen

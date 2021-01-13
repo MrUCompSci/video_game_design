b03375b630512fd579b114c3c92181335cdf8103
MrUCompSci <58056495+MrUCompSci@users.noreply.github.com>
2021-01-13 07:03:56 -0500

    Create growing_circle.js

---
+++ animation_and_games/random_circles/growing_circle.js
@@ -0,0 +1,44 @@
+/* Constants */
+var START_RADIUS = 1;
+var INCREMENT = 1;
+var CHANGE_COLORS_AT = 10;
+
+//Create a new variable called circle, do not set it equal to anything
+
+/* This program draws a growing circle. */
+function start(){
+    /*Set variable circle equal to a new Circle
+    with the global variable START_RADIUS as the
+    aegument*/
+    
+    
+    /*Set the position of the circle to the
+    middle of the screen*/
+    
+    
+    //Add circle to screen
+    
+    
+    /*Set a timer for your growCircle function
+    with 50 as the seconds argument*/
+    
+}
+
+/*Create a new funciton called growCircle
+with no parameters*/
+    //INSIDE THE FUNCTION
+    
+    /*Set the radius of the circle to it's current radius
+    plus the global variable INCREMENT*/
+    
+    
+    
+    /*Create an if statement where
+    circle.getRadius() % CHANGE_COLORS_AT == 0 and if it's 
+    true set the circle to a random new color using
+    the Randomizer*/
+    
+	
+    /*Create an if statement where if double the radius of the circle
+    is greater than or equal to the height of the screen then stop
+    the timer for growCircle

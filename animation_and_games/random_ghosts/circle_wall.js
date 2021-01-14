453da1462149fd556e2e199a2f8ffb8fe3c8f9aa
MrUCompSci <58056495+MrUCompSci@users.noreply.github.com>
2021-01-14 07:51:34 -0500

    Create circle_wall.js

---
+++ animation_and_games/random_ghosts/circle_wall.js
@@ -0,0 +1,53 @@
+var RADIUS = 20;
+var DELAY = 40;
+//Create a variable yPos and set it equal to RADIUS
+
+//Create a variabled called yPos and set it equal to RADIUS
+
+//Create a variable called counter and set it equal to 0
+
+
+function start(){
+  /*Set a timer with arguments draw
+  and DELAY
+	
+}
+
+/*Create new function called draw
+with no parameters*/
+
+  //INSIDE THE FUNCTION
+  
+  /*Create a new variable c and
+  set it equal to a new circle with argument RADIUS*/
+	
+  
+  //Set the position of c to xpos, ypos
+	
+  
+  /*Create an if statement that checks if counter
+  is odd or even, so (counter % 2 == 0)
+      //INSIDE THE IF STATEMENT
+      //Set color of c to red
+
+  
+  //Increment counter by 1
+	
+  
+  //Add c to the canvas
+	
+  
+  //Increment xPos by 2 times the global variable RADIUS
+	
+	
+  //Create an if statement to check if xPos is greater than width of canvas
+	
+    //INSIDE THE IF STATEMENT
+    //Set xPos equal to global RADIUS
+		
+    //Increment yPos by 2 times the global variable RADIUS
+
+
+  //Create an if statement to check if yPos is greater than height of canvas
+	  //INSIDE IF STATEMENT
+    //Stope the timer for draw

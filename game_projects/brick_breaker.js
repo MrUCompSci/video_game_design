a54ccceff7619a9d0cff1c759e70798c8fd4465a
MrUCompSci <58056495+MrUCompSci@users.noreply.github.com>
2021-03-03 08:46:29 -0500

    Create brick_breaker.js

---
+++ game_projects/brick_breaker.js
@@ -0,0 +1,55 @@
+/* Constants for bricks */
+var NUM_ROWS = 8;
+var BRICK_TOP_OFFSET = 10;
+var BRICK_SPACING = 2;
+var NUM_BRICKS_PER_ROW = 10;
+var BRICK_HEIGHT = 10;
+var SPACE_FOR_BRICKS = getWidth() - (NUM_BRICKS_PER_ROW + 1) * BRICK_SPACING;
+var BRICK_WIDTH = SPACE_FOR_BRICKS / NUM_BRICKS_PER_ROW;
+
+/* Constants for ball and paddle */
+var PADDLE_WIDTH = 80;
+var PADDLE_HEIGHT = 15;
+var PADDLE_OFFSET = 10;
+
+var BALL_RADIUS = 15;
+
+/*
+Start function to become the game
+*/
+function start(){
+
+}
+
+/*
+Create a function called drawBrick with 3 parametera
+x, y, color.
+
+Function will draw a colored brick at given position
+*/
+
+
+/*
+Create a function called getColorForRow with 1 parameter
+rownum.
+
+Function will return a color for a specific row.
+*/
+
+
+/*
+Create a function drawRow with 2 parameters
+rowNum, yPos
+
+This function will use our drawBrick function to
+create a single row of bricks.
+*/
+
+
+/*
+Create a function called drawBricks with no
+parameters.
+
+This function will use every function we've elarned so far
+to draw our map.
+*/

653f845c4b6c96a0f2bb2ff930a17f35a4b0ee98
MrUCompSci <58056495+MrUCompSci@users.noreply.github.com>
2021-03-09 07:20:29 -0500

    Create brick_breaker_2.js

---
+++ game_projects/brick_breaker_2.js
@@ -0,0 +1,54 @@
+/*
+Create a function called setUpBall with
+no parameters.
+
+This function will draw and add our ball
+*/
+
+
+/*
+Create a function called setUpPaddle with
+no parameters.
+
+This function will draw and add our paddle.
+*/
+
+
+/*
+Create a function called setSpeeds with
+no parameters.
+
+This function will set the speed of our game
+*/
+
+
+/*
+Create a function called setUp with
+no parameters.
+
+This function will setup our entire game
+*/
+
+
+/*
+Create a function called checkWells with
+no parameters.
+
+This function will make our ball/paddle stay in bounds.
+*/
+
+
+/*
+Create a function called draw with
+no parameters.
+
+This function will chek fo walls and move the ball
+*/
+
+
+/*
+Create a function called myMove with one parameter
+event.
+
+This function will help us move our paddle.
+*/

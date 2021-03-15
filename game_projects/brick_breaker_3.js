ffab90793ec3941dd1e665b7bdb066b560e84048
MrUCompSci <58056495+MrUCompSci@users.noreply.github.com>
2021-03-15 08:06:37 -0400

    Create brick_breaker_3.js

---
+++ game_projects/brick_breaker_3.js
@@ -0,0 +1,47 @@
+/*
+Create a function called getCollidingObject with
+no parameters.
+
+The function will look to find what part of the ball
+something is coliding with.
+*/
+
+
+/*
+Create a function called checkObjects with 
+no parameters.
+
+The function will see what object the ball
+is colliding with.
+*/
+
+
+/*
+Create a function called drawGameOver() with
+no parameters.
+
+The function will draw a game over message.
+*/
+
+
+/*
+Create a function called drawGameWon() with
+no parameters.
+
+The function will draw a game won message.
+*/
+
+
+/*Create a function called checkWin() with
+no parameters.
+
+This function will check to see if brick count is
+0
+*/
+
+
+/*Create a function called checkLose() with
+no parameters.
+
+This function will check for he game to be over.
+*/
